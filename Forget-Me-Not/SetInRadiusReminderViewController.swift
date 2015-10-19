f2a644a933410e3cf100a9131c21ba977afccef0
Lukas Carvajal <lukascarvajal@gmail.com>
2015-10-19 02:07:04 -0500

    Update view for no reminders set and set up uipickerview.

--- Forget-Me-Not/SetInRadiusReminderViewController.swift
+++ Forget-Me-Not/SetInRadiusReminderViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import MapKit
 
-class SetInRadiusReminderViewController: UIViewController, UITextFieldDelegate, UIAlertViewDelegate {
+class SetInRadiusReminderViewController: UIViewController, UITextFieldDelegate, UIAlertViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
 
     var notificationCircle = MKCircle()
+    var numbersArray = NSMutableArray()
     
     @IBOutlet weak var reminderTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var titleTextField: UITextField!
@@ -24,6 +25,16 @@ class SetInRadiusReminderViewController: UIViewController, UITextFieldDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        timerPickerView.dataSource = self
+        timerPickerView.delegate = self
+        
+        numbersArray.addObject("title")
+        for index in 0...59 {
+            numbersArray.addObject(index)
+        }
+        // First row is for hour/minute titles.
+        timerPickerView.selectRow(1, inComponent: 0, animated: false)
+        timerPickerView.selectRow(1, inComponent: 1, animated: false)
     }
 
     override func viewWillDisappear(animated: Bool) {
@@ -35,6 +46,8 @@ class SetInRadiusReminderViewController: UIViewController, UITextFieldDelegate, 
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Reminder Actions
+    
     @IBAction func saveReminder(sender: AnyObject) {
         if reminderTypeSegmentedControl.selectedSegmentIndex == 0 {
             // Create a 'When I leave' reminder.
@@ -44,7 +57,7 @@ class SetInRadiusReminderViewController: UIViewController, UITextFieldDelegate, 
             UIApplication.sharedApplication().scheduleLocalNotification(reminder)
             
             // Alert user, reminder has been set.
-            let reminderSetAlert = UIAlertView.init(title: "Reminder Set!", message: "A reminder will go off when you leave the area.", delegate: self, cancelButtonTitle: "Ok")
+            let reminderSetAlert = UIAlertView.init(title: "Reminder Set!", message: "You'll receive a notification when you leave the area.", delegate: self, cancelButtonTitle: "Ok")
             reminderSetAlert.show()
             
             // Go back to map.
@@ -61,12 +74,66 @@ class SetInRadiusReminderViewController: UIViewController, UITextFieldDelegate, 
         if reminderTypeSegmentedControl.selectedSegmentIndex == 0 {
             // 'When I leave' reminder requires no timer.
             timerPickerView.hidden = true
+            titleTextField.returnKeyType = UIReturnKeyType.Go
             titleTextField.becomeFirstResponder()
+            titleTextField.reloadInputViews()
         }
         else if reminderTypeSegmentedControl.selectedSegmentIndex == 1 {
             // 'When I haven't left' reminder requires a timer to be set.
             timerPickerView.hidden = false
+            titleTextField.returnKeyType = UIReturnKeyType.Done
             titleTextField.resignFirstResponder()
+        }
+    }
+    
+    // MARK: - TextField Delegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Keyboard actions change based on reminder.
+        if reminderTypeSegmentedControl.selectedSegmentIndex == 0 {
+            saveReminder(self)
+            return true
+        }
+        else if reminderTypeSegmentedControl.selectedSegmentIndex == 1 {
+            titleTextField.resignFirstResponder()
+            return true
+        }
+        return false
+    }
+    
+    // MARK: - PickerView Data Source and Delegate
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            // Hours.
+            return 25
+        }
+        else if component == 1 {
+            // Minutes.
+            return 61
+        }
+        return 0
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if row == 0 {
+            if component == 0 {
+                return "hours"
+            }
+            else {
+                return "minutes"
+            }
+        }
+        return "\(numbersArray[row])"
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if row == 0 {
+            timerPickerView.selectRow(1, inComponent: component, animated: true)
         }
     }
 }
